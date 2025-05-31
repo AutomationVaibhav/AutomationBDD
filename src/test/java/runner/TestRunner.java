146380369427e4891d5951d07fa2c8e6fd4b971b
AutomationVaibhav <corporatevaibhav24@gmail.com>
2025-05-31 20:08:02 +0530

    Updated TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,4 +1,5 @@
 package runner;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -8,7 +9,9 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",   // Path to feature files
         glue = {"stepdefinitions", "Hooks"},         // Correct package names (should match folder names exactly)
         tags = "@Login",                             // Which tag to run
-        plugin = {"pretty", "html:target/cucumber-reports.html"}, // Default + HTML report
+        plugin = {"pretty", "html:target/cucumber-reports.html",
+                "json:target/CucumberTestReport.json"      // For Jenkins plugin!
+        }, // Default + HTML report
         monochrome = true                            // Cleaner console output
 )
 public class TestRunner {
